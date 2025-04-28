1fd1bcae7ff4f3ff1c1687ecc21a24208a48ff7c
Jose Campos <desarrollador8@azsmart.net>
2025-04-28 11:43:43 -0500

    feat: Refactor AddressViewSet para tener el CRUD y adicional poder filtrar direcciones por client_id

--- locations/views.py
+++ locations/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-# Create your views here.
+# locations/views.py
+
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
+
+from accounts.models import Client
 
 from .models import Address
 from .serializers import AddressSerializer
@@ -9,3 +13,17 @@ from .serializers import AddressSerializer
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        client_id = self.kwargs.get('client_id')
+        if not client_id:
+            client_id = self.request.query_params.get('client_id')
+        if client_id:
+            qs = qs.filter(client_id=client_id)
+        return qs
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
