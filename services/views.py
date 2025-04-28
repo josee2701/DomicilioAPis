d8a536fe40e884b2c0f58daa4b6a6a74f8f95357
Jose Campos <desarrollador8@azsmart.net>
2025-04-28 12:06:55 -0500

    feat: Agregar filtrado por client_id en ServiceViewSet y acciones para el conducto para indicar que esta en tramite y completar servicios

--- services/views.py
+++ services/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404, render
+# services/views.py
+from django.utils import timezone
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
 from .models import Service
@@ -11,8 +16,26 @@ class ServiceViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceSerializer
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        client_id = self.request.query_params.get('client_id', None)
+        qs = super().get_queryset()
+        client_id = self.request.query_params.get('client_id')
         if client_id:
-            queryset = queryset.filter(client__id=client_id)
-        return queryset
+            qs = qs.filter(client__id=client_id)
+        return qs
+
+    @action(detail=True, methods=['post'], url_path='start-transit')
+    def start_transit(self, request, pk=None):
+      
+        service = self.get_object()
+        service.status = 'in_transit'
+        service.save(update_fields=['status'])
+        return Response(self.get_serializer(service).data)
+
+    @action(detail=True, methods=['post'], url_path='complete')
+    def complete(self, request, pk=None):
+      
+        service = self.get_object()
+
+        service.status = 'completed'
+        service.date_completed = timezone.now()
+        service.save(update_fields=['status', 'date_completed'])
+        return Response(self.get_serializer(service).data)
